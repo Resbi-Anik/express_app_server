41309bac354f0ef22317d6d7a67f72adfd19089c
Resbi-Anik <rizvianikcse13@gmail.com>
2020-07-07 23:29:00 +0600

    get route

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.get("/", (req, res) => {
+  console.log('get the data');
+});
 app.listen(3000, (req, res) => {
   console.log("hello world");
 });
