2bd7c2c31a505562ad0c4a96740bca2466a3f539
Resbi-Anik <rizvianikcse13@gmail.com>
2020-07-07 23:34:05 +0600

    movie list update

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ var app = express();
 app.get("/", (req, res) => {
   console.log('get the data');
 });
+
+app.get('/movie',(req,res)=>{
+    console.log('list');
+    
+})
 app.listen(3000, (req, res) => {
   console.log("hello world");
 });
